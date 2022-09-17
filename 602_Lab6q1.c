c1fe8ab116aa27ab4c7ffa3e4cee70eee694764d
Pakkapon602 <113761311+Pakkapon602@users.noreply.github.com>
2022-09-17 18:28:51 +0700

    Create 602_Lab6q1.c

---
+++ 602_Lab6q1.c
@@ -0,0 +1,45 @@
+//q2.5 มีผลลัพธ์สองแบบ เมื่อไม่มี join คือ thread ลูกมาก่อนแม่ และ thread แม่มาก่อนลูก
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int  msum;
+int csum;
+void *runner(void *param);
+
+        int main(int argc, char *argv[]){
+                pthread_t tid;
+                pthread_attr_t attr;
+                pthread_attr_init(&attr);
+
+                pthread_create(&tid, &attr,runner, argv[1]);
+
+                int upper = atoi(argv[1]);
+                int i;
+                msum = 0;
+                if(upper >0){
+                        for(i=0;i<=upper;i++){
+                                        msum+=i;
+                                }
+                        }
+
+
+                printf("differece between csum and msum = %d\n",csum-msum);
+
+
+
+
+                return 0;
+        }
+
+        void *runner(void *param){
+                int upper = atoi(param);
+                int i ;
+                csum = 0;
+                if(upper > 0){
+                        for(i = 0;i<=upper*2;i++){
+                                csum+=i;
+                        }
+                }
+                pthread_exit(0);
+        }
